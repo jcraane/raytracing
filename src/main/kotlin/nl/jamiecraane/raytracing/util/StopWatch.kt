03107d54fbb43e23e421e8bc69e4df18ef735a43
jamiecraane <jcraane@gmail.com>
2019-02-05 16:43:09 +0100

    Added StopWatch.kt

---
+++ src/main/kotlin/nl/jamiecraane/raytracing/util/StopWatch.kt
@@ -0,0 +1,14 @@
+package nl.jamiecraane.raytracing.util
+
+import java.time.Duration
+
+object StopWatch {
+    /**
+     * Measures and returns the execution time of the given codeblock.
+     */
+    fun timeIt(block: () -> Unit = {}): Duration {
+        val start = System.currentTimeMillis()
+        block()
+        return Duration.ofMillis(System.currentTimeMillis() - start)
+    }
+}
