b540f9898c59ecf272eaf1513f7ce6ffb7a5751b
jamiecraane <jcraane@gmail.com>
2019-03-19 09:50:11 +0100

    Formatting

--- src/main/kotlin/nl/jamiecraane/raytracing/extensions/Color.kt
+++ src/main/kotlin/nl/jamiecraane/raytracing/extensions/Color.kt
@@ -5,4 +5,4 @@ import java.awt.Color
 operator fun Color.component1() = getColorComponents(FloatArray(4))[0]
 operator fun Color.component2() = getColorComponents(FloatArray(4))[1]
 operator fun Color.component3() = getColorComponents(FloatArray(4))[2]
-operator  fun Color.component4() = getColorComponents(FloatArray(4))[3]
+operator fun Color.component4() = getColorComponents(FloatArray(4))[3]
