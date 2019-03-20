67aced0ea98248ef8862ade5519e2cc848f72055
jamiecraane <jcraane@gmail.com>
2019-03-20 16:37:20 +0100

    Removed default value from block.

--- src/main/kotlin/nl/jamiecraane/raytracing/util/StopWatch.kt
+++ src/main/kotlin/nl/jamiecraane/raytracing/util/StopWatch.kt
@@ -6,7 +6,7 @@ object StopWatch {
     /**
      * Measures and returns the execution time of the given codeblock.
      */
-    fun timeIt(block: () -> Unit = {}): Duration {
+    fun timeIt(block: () -> Unit): Duration {
         val start = System.currentTimeMillis()
         block()
         return Duration.ofMillis(System.currentTimeMillis() - start)
