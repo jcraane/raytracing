b3763d64a3aebbf1c894a11f4e189b81cc8368fe
jamiecraane <jcraane@gmail.com>
2019-05-04 09:44:47 +0200

    Set delegate default to emptyList()

--- src/main/kotlin/nl/jamiecraane/raytracing/util/BoundedList.kt
+++ src/main/kotlin/nl/jamiecraane/raytracing/util/BoundedList.kt
@@ -3,7 +3,7 @@ package nl.jamiecraane.raytracing.util
 /**
  * Very simple (and not correct because only the add(element) is implemented) delegation of MutableList which is bounded by maxCapacity.
  */
-class BoundedList<E>(private val maxCapacity: Int, private val list: MutableList<E>) : MutableList<E> by list {
+class BoundedList<E>(private val maxCapacity: Int, private val list: MutableList<E> = mutableListOf()) : MutableList<E> by list {
     override fun add(element: E): Boolean {
         if (size < maxCapacity) {
             list.add(element)
