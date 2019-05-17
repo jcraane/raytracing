9c4766fee5bec87544cd4da13c364c56468492f0
jamiecraane <jcraane@gmail.com>
2019-05-17 12:03:19 +0200

    Fix compile error

--- src/main/kotlin/nl/jamiecraane/raytracing/renderingsamples/SimpleScene.kt
+++ src/main/kotlin/nl/jamiecraane/raytracing/renderingsamples/SimpleScene.kt
@@ -8,7 +8,7 @@ val simpleScene = scene {
     sphere(center = Vect3(-1.0, -1.5, -12.0), material = glass) { radius = 2.0 }
     sphere(center = Vect3(1.5, -0.5, -18.0), material = redRubber) { radius = 3.0 }
     sphere(center = Vect3(7.0, 5.0, -18.0), material = mirror) { radius = 4.0 }
-//    sphere(center = Vect3(4.0, -1.5.0, -13.0), material = mirror) { radius = 3.0 }
+//    sphere(center = Vect3(4.0, -1.5, -13.0), material = mirror) { radius = 3.0 }
 
     light(Vect3(-20.0, 20.0, 20.0)) { intensity = 1.5 }
     light(Vect3(30.0, 50.0, -25.0)) { intensity = 1.8 }
